d44f6211526066d7c1809276a8a1dc231c7c7cce
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-26 15:33:04 +0300

    add-task-and-test-project

---
+++ NullableTypeExtensions.Tests/NullableTypeExtensionsTests.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Text;
+using NullableTypesTask;
+using NUnit.Framework;
+
+namespace NullableTypeExtensions.Tests
+{
+    [TestFixture]
+    public class HiddenTest
+    {
+        private static IEnumerable<TestCaseData> DataCases
+        {
+            get
+            {
+                yield return new TestCaseData("Kathy").Returns(false);
+                yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData(new object()).Returns(false);
+                yield return new TestCaseData(new StringBuilder()).Returns(false);
+                yield return new TestCaseData(null).Returns(true);
+                yield return new TestCaseData(new char?('t')).Returns(false);
+            }
+        }
+
+        [TestCaseSource(nameof(DataCases))]
+        public bool IsNullTests(object variable) => variable.IsNull();
+
+        [Test]
+        public void IsNull_With_Nullable_Integer_Return_True()
+        {
+            int? x = null;
+            Assert.True(x.IsNull());
+        }
+
+        [Test]
+        public void IsNull_With_Nullable_Integer_Return_False()
+        {
+            int? x = 12;
+            Assert.False(x.IsNull());
+        }
+    }
+}
---
+++ NullableTypesTask/NullableTypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullableTypesTask
+{
+    public static class NullableTypeExtensions
+    {
+        public static bool IsNull(this object variable)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
