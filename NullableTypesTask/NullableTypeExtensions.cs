2afb6a1d8b87f72bd8463298d567f75654404347
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-06-01 15:43:26 +0000

    Update NullableTypeExtensions.cs

--- NullableTypesTask/NullableTypeExtensions.cs
+++ NullableTypesTask/NullableTypeExtensions.cs
@@ -4,6 +4,12 @@ namespace NullableTypesTask
 {
     public static class NullableTypeExtensions
     {
+        /// <summary>
+        /// Implement for nullable types the additional possibility of determining whether
+        /// the nullable variable is null or not.
+        /// </summary>
+        /// <param name="variable"></param>
+        /// <returns>True if variable is null, false otherwise</returns>
         public static bool IsNull(this object variable)
         {
             throw new NotImplementedException("You need to implement this function.");
