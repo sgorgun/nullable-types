ccfa82ab2c4a29b89c8dab7d2d2542d6b2f37bff
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2022-02-08 20:11:47 +0300

    Fix some style settings.

--- NullableTypesTask/NullableTypeExtensions.cs
+++ NullableTypesTask/NullableTypeExtensions.cs
@@ -1,15 +1,18 @@
-using System;
+﻿using System;
 
 namespace NullableTypesTask
 {
+    /// <summary>
+    /// Nullable type extension class.
+    /// </summary>
     public static class NullableTypeExtensions
     {
         /// <summary>
         /// Implement for nullable types the additional possibility of determining whether
         /// the nullable variable is null or not.
         /// </summary>
-        /// <param name="variable"></param>
-        /// <returns>true if variable is null, false otherwise</returns>
+        /// <param name="variable">Source parameter.</param>
+        /// <returns>true if variable is null, false otherwise.</returns>
         public static bool IsNull(this object? variable)
         {
             throw new NotImplementedException("You need to implement this method.");
